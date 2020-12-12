8bfc4a35c8b98ec6bd8c958ac37c753311667acb
yoshi0202 <kobayashi020202@gmail.com>
2020-12-12 16:55:13 +0900

    feat: remove start button

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import {Example}  from "./example";
 
 const example:Example = new Example("test", 30);
-const roundBtn:HTMLElement | null = document.getElementById("round_start");
 const roundImg:HTMLElement | null = document.getElementById("roundImg");
 const hiddenImg:Element = document.getElementsByClassName("roundImg")[0];
-roundBtn && roundImg && hiddenImg && (roundBtn.onclick = function() {
+roundImg && hiddenImg && (roundImg.onclick = function() {
   roundImg.style.display = "none"
   hiddenImg.setAttribute("style", "display: block;")
 })

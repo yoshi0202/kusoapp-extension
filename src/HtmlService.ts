bb72968114c8fc1592883e90ce43fca52ddf5065
yoshi0202 <kobayashi020202@gmail.com>
2020-12-12 21:07:11 +0900

    feat: create rotate pop

--- src/HtmlService.ts
+++ src/HtmlService.ts
@@ -1,17 +1,27 @@
 export class HtmlService {
   private buttonCount: number = 0
+  private initSpeed: number = 10
   protected maxCount: number = 10
   
   setCount(num: number): void {
     this.buttonCount = num 
   }
 
-  addCount(): void {
-    this.buttonCount += 1
+  addCount(num: number): void {
+    this.buttonCount += num
   }
 
   getCount(): number {
     return this.buttonCount
+  }
+
+  setSpeed(num: number): void {
+    this.initSpeed = num 
+  }
+
+  divSpeed(): void {
+    this.initSpeed = this.initSpeed / 2
+    console.log(this.initSpeed)
   }
 
   getMaxCount(): number {
@@ -20,6 +30,15 @@ export class HtmlService {
 
   changeDisplayStyle(elem: HTMLElement, style: string) {
     elem.style.display = style
+  }
+
+  changeRotateSpeedToElement(elem: HTMLElement) {
+    elem.style.animation = `rotation ${this.initSpeed}s infinite linear`
+  }
+
+  toggleDisplayImg(noneElm: HTMLElement, blockElm: HTMLElement) {
+    this.changeDisplayStyle(noneElm, "none")
+    this.changeDisplayStyle(blockElm, "block")
   }
 
 }
--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,14 @@ const wrapper:HTMLElement | null = document.querySelector("#content_wrapper")
 const pngImg:HTMLElement | null = document.querySelector(".round_png")
 const gifImg:HTMLElement | null = document.querySelector(".round_gif")
 wrapper && pngImg && gifImg && (wrapper.onclick = function() {
-  htmlService.addCount()
+  htmlService.addCount(1)
   if(htmlService.getCount() === htmlService.getMaxCount()) {
     htmlService.setCount(0)
-    htmlService.changeDisplayStyle(pngImg, "none")
-    htmlService.changeDisplayStyle(gifImg, "block")
+    htmlService.setSpeed(10)
+    htmlService.toggleDisplayImg(pngImg, gifImg)
   } else {
-    htmlService.changeDisplayStyle(pngImg, "block")
-    htmlService.changeDisplayStyle(gifImg, "none")
+    htmlService.toggleDisplayImg(gifImg, pngImg)
+    htmlService.divSpeed()
+    htmlService.changeRotateSpeedToElement(pngImg)
   }
-  // roundImg.style.display = "none"
-  // hiddenImg.setAttribute("style", "display: block;")
-  console.log(htmlService.getCount())
 })

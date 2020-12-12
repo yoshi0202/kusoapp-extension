800280279a592c040974ed50a799b6a6909885b8
yoshi0202 <kobayashi020202@gmail.com>
2020-12-12 17:54:38 +0900

    refactor: app.ts by htmlService.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
-import {Example}  from "./example";
+import {HtmlService}  from "./HtmlService";
 
-const example:Example = new Example("test", 30);
-const roundImg:HTMLElement | null = document.getElementById("roundImg");
-const hiddenImg:Element = document.getElementsByClassName("roundImg")[0];
-roundImg && hiddenImg && (roundImg.onclick = function() {
-  roundImg.style.display = "none"
-  hiddenImg.setAttribute("style", "display: block;")
+const htmlService:HtmlService = new HtmlService()
+const wrapper:HTMLElement | null = document.querySelector("#content_wrapper")
+const pngImg:HTMLElement | null = document.querySelector(".round_png")
+const gifImg:HTMLElement | null = document.querySelector(".round_gif")
+wrapper && pngImg && gifImg && (wrapper.onclick = function() {
+  htmlService.addCount()
+  if(htmlService.getCount() === htmlService.getMaxCount()) {
+    htmlService.setCount(0)
+    htmlService.changeDisplayStyle(pngImg, "none")
+    htmlService.changeDisplayStyle(gifImg, "block")
+  } else {
+    htmlService.changeDisplayStyle(pngImg, "block")
+    htmlService.changeDisplayStyle(gifImg, "none")
+  }
+  // roundImg.style.display = "none"
+  // hiddenImg.setAttribute("style", "display: block;")
+  console.log(htmlService.getCount())
 })

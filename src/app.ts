44c955a4fd1538d7104544e11ed36c1506effa38
yoshi0202 <kobayashi020202@gmail.com>
2020-12-12 16:40:00 +0900

    feat: add round start button

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import {Example}  from "./example";
 
 const example:Example = new Example("test", 30);
+const roundBtn:HTMLElement | null = document.getElementById("round_start");
 const roundImg:HTMLElement | null = document.getElementById("roundImg");
 const hiddenImg:Element = document.getElementsByClassName("roundImg")[0];
-roundImg && hiddenImg && (roundImg.onclick = function() {
+roundBtn && roundImg && hiddenImg && (roundBtn.onclick = function() {
   roundImg.style.display = "none"
   hiddenImg.setAttribute("style", "display: block;")
 })
